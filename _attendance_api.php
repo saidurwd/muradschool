ad6be0c4a188ad771d1c4549afb65a136f4be3bd
Saidur Rahman <saidurwd@gmail.com>
2018-03-05 00:25:09 +0600

    Time fix

--- _attendance_api.php
+++ _attendance_api.php
@@ -4,7 +4,8 @@ include('dbconnect.php');
 
 //formating date
 //$date = new DateTime();
-$date = new DateTime('now', new DateTimezone('Asia/Dhaka'));
+//$date = new DateTime('now', new DateTimezone('Asia/Dhaka'));
+$date = new DateTime('now', new DateTimezone('UTC'));
 $date->setTimestamp(@$_REQUEST['date_time']);
 
 $user_did = @$_REQUEST['card_no'];
